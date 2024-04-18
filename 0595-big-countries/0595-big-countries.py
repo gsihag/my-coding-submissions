b70aaf9685803bbd537b994631ea0d19f3c8ecfe
Gulshan Sihag <46778138+gsihag@users.noreply.github.com>
2024-04-18 09:50:49 +0200

    Time: 519 ms (88.48%), Space: 68.2 MB (16.93%) - LeetHub

--- 0595-big-countries/0595-big-countries.py
+++ 0595-big-countries/0595-big-countries.py
@@ -1,7 +1,4 @@
 import pandas as pd
 
 def big_countries(world: pd.DataFrame) -> pd.DataFrame:
-    df_area = pd.DataFrame(world[world['area']>= 3000000])
-    df_population = pd.DataFrame(world[world['population']>= 25000000])
-    df = pd.concat([df_area, df_population]).drop_duplicates(subset='name')
-    return df[['name', 'population', 'area']]
+    return world[(world.area>= 3000000) | (world.population>= 25000000)][['name', 'population', 'area']]
