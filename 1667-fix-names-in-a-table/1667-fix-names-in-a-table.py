dae08c480115f7c2f6e7c0b913215f1d3dc29697
Gulshan Sihag <46778138+gsihag@users.noreply.github.com>
2024-04-21 15:43:47 +0200

    Time: 669 ms (48.95%), Space: 66.7 MB (66.4%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.py
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.py
@@ -1,13 +1,5 @@
 import pandas as pd
 
 def fix_names(users: pd.DataFrame) -> pd.DataFrame:
-    for idx,name_str in users['name'].items():
-        #print(name_str, len(name_str))
-        first_let = name_str[0].upper()
-        let_rest = name_str[1:].lower()
-        corrected_name = first_let+let_rest
-        #print(corrected_name, len(corrected_name))
-        users['name'][idx] = corrected_name
-    
-    users.sort_values(by='user_id', inplace=True)
-    return users
+    users['name'] = users['name'].str.capitalize()
+    return users.sort_values(by='user_id')
