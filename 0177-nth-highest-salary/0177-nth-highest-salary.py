d15e336e98d1ee375379868edecaf07924c85ee5
Gulshan Sihag <46778138+gsihag@users.noreply.github.com>
2024-04-22 16:01:24 +0200

    Time: 604 ms (59.89%), Space: 65.5 MB (78.87%) - LeetHub

---
+++ 0177-nth-highest-salary/0177-nth-highest-salary.py
@@ -0,0 +1,17 @@
+import pandas as pd
+
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    sal = employee['salary'].drop_duplicates().tolist()
+    sal = sorted(sal, reverse=True)
+    print(sal)
+    res = {"getNthHighestSalary({n})".format(n=N):[]}
+    if N <= 0:
+        res["getNthHighestSalary({n})".format(n=N)].append(None)
+    else:
+        if N > len(sal):
+            res["getNthHighestSalary({n})".format(n=N)].append(None)
+        else:
+            res["getNthHighestSalary({n})".format(n=N)].append(sal[N-1])
+
+    print(res)
+    return pd.DataFrame(res)
