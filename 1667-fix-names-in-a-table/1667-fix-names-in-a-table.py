e8a62ff873a4edac97b76b77813b98c404cc614d
Gulshan Sihag <46778138+gsihag@users.noreply.github.com>
2024-04-25 09:38:38 +0200

    Time: 889 ms (5.05%), Space: 66.5 MB (71.6%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.py
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.py
@@ -1,5 +1,11 @@
+# Import the pandas library as pd
 import pandas as pd
 
+# Define a function called fix_names that takes a pandas DataFrame as input
 def fix_names(users: pd.DataFrame) -> pd.DataFrame:
+    
+    # Capitalize the 'name' column in the DataFrame using the str.capitalize() method
     users['name'] = users['name'].str.capitalize()
+    
+    # Sort the DataFrame by the 'user_id' column in ascending order
     return users.sort_values(by='user_id')
